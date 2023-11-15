b1c8b3e1c63dfa4a38640736cebb45de512435d6
ptitpoulpe <ptitpoulpe@ptitpoulpe.fr>
2023-11-15 11:23:35 +0100

    Time: 4130 ms (5.01%) | Memory: 16.5 MB (16.53%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.py
@@ -0,0 +1,35 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums_set = set(nums)
+        nums = sorted(nums)
+        res = 10**6
+        best_diff = 10**6
+        #print(nums)
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                s = nums[i] + nums[j]
+                diff = target - s
+                min_k = 0
+                max_k = len(nums) - 1
+                while max_k - min_k != 1:
+                    mid = min_k + (max_k - min_k) // 2
+                    if nums[mid] < diff:
+                        min_k = mid
+                    else:
+                        max_k = mid
+                if min_k in (i, j):
+                    min_k -= 1
+                if max_k in (i, j):
+                    max_k += 1
+
+                #print(i, j, s, diff, min_k, max_k)
+                if min_k >= 0 and (min_k_diff := abs(diff - nums[min_k])) < best_diff:
+                    best_diff = min_k_diff
+                    res = s + nums[min_k]
+                    #print("min: ", res)
+                
+                if max_k < len(nums) and (max_k_diff := abs(diff - nums[max_k])) < best_diff:
+                    best_diff = max_k_diff
+                    res = s + nums[max_k]
+                    #print("max: ", res)     
+        return res
