6f6955ee21b2356d322419a91bd91a0e05e15589
ptitpoulpe <ptitpoulpe@ptitpoulpe.fr>
2023-11-21 14:07:54 +0100

    Time: 100 ms (7.22%) | Memory: 16.7 MB (13.50%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        res = ListNode(next=head)
+        queue = res
+        while True:
+            print(queue)
+            if queue.next is None or queue.next.next is None:
+                break
+            tmp = queue.next
+            queue.next = queue.next.next
+            tmp.next = queue.next.next
+            queue.next.next = tmp
+            queue = queue.next.next
+        return res.next
